c8d65626589666b098d77c70cc901dcf3c47b750
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 17:30:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,8 @@ function nowServing(katzDeliLine){
   if(katzDeliLine.length==0){
     return "There is nobody waiting to be served!"
   }
-
+  else{
+    name = katzDeliLine.upshift()
+    
+  }
 }
