2dc666daf83553e1c7bdb3ea819ee05b2c50b8be
Cayla Schwartz <cayschwartz@gmail.com>
2017-09-12 17:39:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if(katzDeliLine.length==0){
-    return "There line is currently empty."
+    return "The line is currently empty."
   }
   else{
     var line = "The line is currently: "
